de186e5b0df94d909f340ac64c408e2e1517190d
Paul Rukwaro <rukwaropaul12@gmail.com>
2022-04-26 20:51:20 +0300

    When given a list of lists, this fn makes a dictionary representing the graph

--- Graph Data Structures/building_a_graph.py
+++ Graph Data Structures/building_a_graph.py
@@ -6,18 +6,19 @@ def build_graph(nodes):
     """
     The function makes an adjacency list
     which in python is a dict. Key value pairing
+    NOTE: this is a undirected graph
     :param nodes: A list of lists
     :return: dictionary data type
     """
     graph = {}
     for node in nodes:
         [key, value] = node
-        if key not in graph:
+        if key not in graph:  # Key is a list
             graph[key] = []
         if value not in graph:
             graph[value] = []
         graph[key].append(value)
-        graph[value].append(key)
+        graph[value].append(key)  # omit this for directional graph
     return graph
 
 
