14e5bc45399ae9910fd3aebb0a48f1fa75c29a05
Paul Rukwaro <rukwaropaul12@gmail.com>
2022-04-27 19:54:53 +0300

    Foundational implementation of a linked list

--- Linked_List_Data_structures/Linked_list_implementation.py
+++ Linked_List_Data_structures/Linked_list_implementation.py
@@ -1,5 +1,9 @@
 # Linked List implementation
 class Node:
+    """
+    The data is what we're actually storing while
+    next is the pointer to the next data. Stored as one package
+    """
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
@@ -11,3 +15,10 @@ for i in range(len(lst) - 1):
     ll.data = lst[i]
     ll.next = lst[i + 1]
     print(f"{ll.data} --> {ll.next}")
+
+"""
+A --> B
+B --> C
+C --> D
+D --> E
+"""
